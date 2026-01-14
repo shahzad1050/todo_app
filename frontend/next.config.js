d3ed0e3b7b66655319ffadda7eed1388ae109366
shahzad1050 <shahzadarham266@gmail.com>
2026-01-15 03:28:19 +0500

    Remove basePath and assetPrefix from Next.js config for Vercel deployment

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,8 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export', // Enables static exports for GitHub Pages
-  basePath: '/Todo',
-  assetPrefix: '/Todo/',
   images: {
     unoptimized: true,
   },
